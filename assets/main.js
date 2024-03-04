66e99800689da0a475178953506bac37d23f5b84
YuanS <vir.suy@gmail.com>
2024-03-04 13:10:47 -0500

    refactor

--- assets/main.js
+++ assets/main.js
@@ -1,47 +1,47 @@
-function removeRow() {
-  event.target.parentElement.remove();
-}
-
-function addToPrint(character) {
-  const printArea = document.getElementById("characters");
-  const printRow = document.createElement("div");
-  printRow.classList.add("row");
-  for (i = 0; i < 3; i++) {
-    printRow.innerHTML += `<span class="char lead"><span class="letter">${character}</span></span>`;
+$(document).ready(function () {
+  function removeRow() {
+    $(event.target).parent().remove();
   }
-  for (i = 0; i < 12; i++) {
-    printRow.innerHTML += `<span class="char practice"><span class="letter">${character}</span></span>`;
+
+  function addToPrint(character) {
+    var printArea = $("#characters");
+    var printRow = $("<div></div>").addClass("row");
+    for (var i = 0; i < 3; i++) {
+      printRow.append(
+        `<span class="char lead"><span class="letter">${character}</span></span>`
+      );
+    }
+    for (var i = 0; i < 12; i++) {
+      printRow.append(
+        `<span class="char practice"><span class="letter">${character}</span></span>`
+      );
+    }
+    printRow.append(`<button class="delete noPrint">x</button>`);
+    printRow.append(`<div class="divider">divider</div>`);
+    printArea.append(printRow);
+    printRow.find(".delete").click(removeRow);
   }
-  printRow.innerHTML += `<button class="delete noPrint" onClick="removeRow()">x</button>`;
-  printRow.innerHTML += `<div class="divider">divider</div>`;
-  printArea.appendChild(printRow);
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("data/levels.json")
-    .then((response) => response.json())
-    .then((data) => {
-      for (const level in data) {
-        // level, levelData
-        const levelData = data[level];
+  $.getJSON("data/levels.json", function (data) {
+    $.each(data, function (level, levelData) {
+      var levelDiv = $("<div></div>").addClass("level");
+      levelDiv.html(`
+              <h4>Grade ${level}</h4>
+              <div class="level-content" id="${level}"></div>
+          `);
 
-        const levelDiv = document.createElement("div");
-        levelDiv.classList.add("level");
-        levelDiv.innerHTML = `
-            <h4>Grade ${level}</h4>
-            <div class="level-content" id=${level}></div>
-            `;
+      $("#levels").append(levelDiv);
 
-        document.getElementById("levels").appendChild(levelDiv);
-
-        levelData.forEach((character) => {
-          const characterSpan = document.createElement("span");
-          characterSpan.classList.add("character");
-          characterSpan.innerHTML = `
-                <button class="character" onclick="addToPrint('${character}')">${character}</button>
-                `;
-          document.getElementById(level).appendChild(characterSpan);
+      $.each(levelData, function (index, character) {
+        var characterSpan = $("<span></span>").addClass("character");
+        characterSpan.html(`
+                  <button class="character">${character}</button>
+              `);
+        characterSpan.find("button").click(function () {
+          addToPrint(character);
         });
-      }
+        $("#" + level).append(characterSpan);
+      });
     });
+  });
 });
