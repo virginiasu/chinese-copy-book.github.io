7a47464aa163e93f73627f6100b4b7a16e33c01a
YuanS <vir.suy@gmail.com>
2024-03-04 15:59:07 -0500

    add function to pick all

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,39 @@
+function pickAll() {
+  const level = $(event.target).parent().parent();
+  level.find("button.character").each(function () {
+    addToPrint($(this).text());
+  });
+}
+
+function addToPrint(character) {
+  var printArea = $("#characters");
+  var printRow = $("<div></div>").addClass("row");
+  for (var i = 0; i < 3; i++) {
+    printRow.append(
+      `<span class="char lead"><span class="letter">${character}</span></span>`
+    );
+  }
+  for (var i = 0; i < 12; i++) {
+    printRow.append(
+      `<span class="char practice"><span class="letter">${character}</span></span>`
+    );
+  }
+  printRow.append(`<button class="delete noPrint">x</button>`);
+  printRow.append(`<div class="divider">divider</div>`);
+  printArea.append(printRow);
+  printRow.find(".delete").click(removeRow);
+}
+
+function removeRow() {
+  $(event.target).parent().remove();
+}
+
 $(document).ready(function () {
-  function removeRow() {
-    $(event.target).parent().remove();
-  }
-
-  function addToPrint(character) {
-    var printArea = $("#characters");
-    var printRow = $("<div></div>").addClass("row");
-    for (var i = 0; i < 3; i++) {
-      printRow.append(
-        `<span class="char lead"><span class="letter">${character}</span></span>`
-      );
-    }
-    for (var i = 0; i < 12; i++) {
-      printRow.append(
-        `<span class="char practice"><span class="letter">${character}</span></span>`
-      );
-    }
-    printRow.append(`<button class="delete noPrint">x</button>`);
-    printRow.append(`<div class="divider">divider</div>`);
-    printArea.append(printRow);
-    printRow.find(".delete").click(removeRow);
-  }
-
   $.getJSON("data/levels.json", function (data) {
     $.each(data, function (level, levelData) {
       var levelDiv = $("<div></div>").addClass("level");
       levelDiv.html(`
-              <h4>Grade ${level}</h4>
+              <h4>Grade ${level} <button class="pickAll" onClick="pickAll()">Pick All</button></h4>
               <div class="level-content" id="${level}"></div>
           `);
 
