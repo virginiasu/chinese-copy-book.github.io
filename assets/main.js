d6e4fa50c88bec5e4131e042ebbbf4f3d517b966
virginiasu <vir.suy@gmail.com>
2025-07-29 23:20:45 -0400

    toggle

--- assets/main.js
+++ assets/main.js
@@ -3,11 +3,37 @@ $(document).ready(function () {
     $.each(data, function (level, levelData) {
       var levelDiv = $("<div></div>").addClass("level");
       levelDiv.html(`
-              <h4>Level ${level} <button class="pickAll" onClick="pickAll()">全选</button></h4>
+              <h4>
+                <span class="accordion-toggle">▶</span>
+                Level ${level} 
+                <button class="pickAll" onClick="pickAll()">全选 Select All</button>
+              </h4>
               <div class="level-content" id="${level}"></div>
           `);
 
       $("#levels").append(levelDiv);
+
+      // Add accordion functionality
+      levelDiv.find("h4").click(function (e) {
+        // Don't trigger accordion when clicking the pickAll button
+        if ($(e.target).hasClass("pickAll")) {
+          return;
+        }
+
+        var content = $(this).siblings(".level-content");
+        var toggle = $(this).find(".accordion-toggle");
+
+        if (content.is(":visible")) {
+          content.slideUp();
+          toggle.text("▶");
+        } else {
+          content.slideDown();
+          toggle.text("▼");
+        }
+      });
+
+      // Initially hide the content
+      $("#" + level).hide();
 
       $.each(levelData, function (index, character) {
         var characterSpan = $("<span></span>").addClass("character");
